8db40bbdfa3862d9525ce62a7b00a2864ffc70f9
Drazen Dodik <drazend@live.fi>
2022-04-12 23:31:06 +0300

    Add action

---
+++ valohai-execution-action/runExecution.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import os
+ 
+# Authenticate yourself with the token.
+# Remember to follow your organization's security standards when handling the token.   
+auth_token = os.getenv('VH_API_TOKEN')
+headers = {'Authorization': 'Token %s' % auth_token}
+project_id = '01801ef9-ca55-dd02-af46-6978e6f2776d'
+step_name = 'train'
+ 
+# Fetch all new changes from the repository
+# https://app.valohai.com/api/docs/#projects-fetch
+# This will fetch changes from all the branches that you've defined on the Project->Settings->Repository tab
+fetchResponse = requests.post(('https://app.valohai.com/api/v0/projects/{0}/fetch/').format(project_id), data={'id': project_id}, headers=headers)
+fetchResponse.raise_for_status()
+ 
+ 
+# Define the payload for a new execution
+# https://app.valohai.com/api/docs/#executions-create
+#
+# GitHub Actions creates an environment variable on the Docker container
+# Called GITHUB_SHA that stores the identifier of the commit that was created
+new_exec_payload = {
+    "project": project_id,
+    "commit": os.getenv('GITHUB_SHA'),
+    "step": step_name
+}
+ 
+# Send a POST request to create a new execution
+createExecutionResponse = requests.post('https://app.valohai.com/api/v0/executions/', data=new_exec_payload, headers=headers)
+createExecutionResponse.raise_for_status()
+ 
+# Print the response you've received back
+print('# API Response:\n')
+print(json.dumps(createExecutionResponse.json(), indent=4))
